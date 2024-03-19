c6fb3ea12c9dbcd23e69439647ea6901f065e5de
Eduardo Schilling <elschilling@gmail.com>
2024-03-19 10:42:32 -0300

    services card

---
+++ components/Card.vue
@@ -0,0 +1,26 @@
+<template>
+<div class="w-[214px] h-[221px] bg-[#3D1914] rounded-b-[20px] mt-10">
+    <div class="bg-[#ff6c53]/30 h-[54px] top-[-27px] relative rounded-full flex">
+        <div class="bg-[#ff6c53] rounded-full w-[54px] h-[54px] flex justify-center items-center">
+            <img src="~/assets/icone-projetos.svg" alt="">          
+        </div>       
+        <div class="flex justify-center items-center font-bold text-white text-[11px] w-[150px] tracking-[.5em]">
+            PROJETOS
+        </div>
+    </div>
+    <div class="text-[14px] text-white flex-col justify-center items-center tracking-normal relative top-[-27px]">
+        <p class="text-center">Arquitetônicos<br>
+        Simplificados<br>
+        Estruturais<br>
+        Elétricos<br>
+        Hidrossanitários<br>
+        Urbanísticos</p>
+    </div>
+</div>
+</template>
+
+<script setup>
+</script>
+
+<style>
+</style>
--- pages/index.vue
+++ pages/index.vue
@@ -19,24 +19,27 @@
         <swiper-slide class="bg-[url('~/assets/lucas.webp')] bg-center bg-cover"></swiper-slide>
     </swiper-container>
 
-    <div class="w-full avida xl:w-[1200px] bg-stone-950 m-auto font-['Lexend'] p-10 text-zinc-400 font-extralight text-2xl antialiased tracking-widest fadeIn ">
-        <p class="py-5">
-            A vida é feita de <span class="font-bold">sonhos</span>. Uns modestos, outros normais e ainda os arrojados.
-        </p>
-        <p class="py-5">
-            O <span class="font-bold">nosso</span> maior sonho é, através de nossos projetos e construções, materializar e ajudar a concretizar os sonhos das pessoas, casais, famílias ou coletividades.
-        </p>
-        <p class="py-5">
-            Permita que, com o nosso trabalho, experiência, competência e dedicação sejamos um pouco responsáveis pela vossa <span class="font-bold">felicidade</span>.
-        </p>
-
-
+    <div class="w-full xl:w-[1200px] bg-stone-950 m-auto font-['Lexend'] p-10 text-zinc-400 font-extralight text-2xl antialiased tracking-widest fadeIn ">
+  
+        <div class="avida">
+            <p class="py-5">
+                A vida é feita de <span class="font-bold">sonhos</span>. Uns modestos, outros normais e ainda os arrojados.
+            </p>
+            <p class="py-5">
+                O <span class="font-bold">nosso</span> maior sonho é, através de nossos projetos e construções, materializar e ajudar a concretizar os sonhos das pessoas, casais, famílias ou coletividades.
+            </p>
+            <p class="py-5">
+                Permita que, com o nosso trabalho, experiência, competência e dedicação sejamos um pouco responsáveis pela vossa <span class="font-bold">felicidade</span>.
+            </p>
+  
+        </div>    
 
         <div class="bg-red-400/50 row-full">
-            <div>
+            <div class="w-full xl:w-[1200px]  m-auto p-5">
+                <Card />
+                <p>lalalalal</p>
                 
             </div>
-            <p>lalalalal</p>
         </div>
 
     </div>
